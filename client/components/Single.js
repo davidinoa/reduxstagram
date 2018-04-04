1970848d5ff147146bc8a40961aab81e4ab5205d
David Inoa <davidinoa@gmail.com>
2018-04-04 16:30:44 -0700

    Create single and photo grid components

---
+++ client/components/Single.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Single extends Component {
+	render() {
+		return <div className="single-photo">I'm the single</div>;
+	}
+}
+
+export default Single;
