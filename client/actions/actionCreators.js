a21eac95bc3bcbf02576e0137aa788db63abee24
David Inoa <davidinoa@gmail.com>
2018-04-04 19:20:08 -0700

    Integrate store with React Router

--- client/actions/actionCreators.js
+++ client/actions/actionCreators.js
@@ -1,5 +1,5 @@
 // increment
-function increment(index) {
+export function increment(index) {
 	return {
 		type: 'INCREMENT_LIKES',
 		index
@@ -7,7 +7,7 @@ function increment(index) {
 }
 
 // add comment
-function addComment(postId, author, comment) {
+export function addComment(postId, author, comment) {
 	return {
 		type: 'ADD_COMMENT',
 		postId,
@@ -17,3 +17,6 @@ function addComment(postId, author, comment) {
 }
 
 // remove comment
+export function removeComment(postId, i) {
+	type: 'REMOVE_COMMENT', postId, i;
+}
